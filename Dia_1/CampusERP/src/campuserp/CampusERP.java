2a77e98b9b6410a31c8eb3a8d4dd2362036a87dc
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-07-22 18:00:26 -0500

    feat: :sparkles: Menu de Campus

--- Dia_1/CampusERP/src/campuserp/CampusERP.java
+++ Dia_1/CampusERP/src/campuserp/CampusERP.java
@@ -8,7 +8,8 @@ package campuserp;
  *
  * @author camper
  */
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class CampusERP {
 
@@ -22,12 +23,43 @@ public class CampusERP {
         boolean salir = false;
         int opcion; //Opcion del usuario
         
-        while)(!salir){
-        
-        
+        while(!salir){ // mientras salir sea true 
+           
+            System.out.println("------Menú-----");
+            System.out.println("1. Coordinador");
+            System.out.println("2. Trainer");
+            System.out.println("3. Camper");
+            System.out.println("4. Salir");
+            
+            try {
+            
+                System.out.println("Escriba una de las opciones");
+                opcion = sn.nextInt(); //recibe el numero entero y lo compara 
+            
+                switch (opcion){
+                    case 1: 
+                        System.out.println("\n --- Entraste a Coordinador --- \n ");
+                        break;
+                    case 2: 
+                        System.out.println(" \n --- Entraste a Trainer --- \n");
+                        break;    
+                    case 3: 
+                        System.out.println("\n --- Entraste a Camper --- \n");
+                        break;
+                    case 4: 
+                        System.out.println("\n --- Saliendo --- \n");
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("\n ---- Opción Incorrecta --- \n ");
+                }
+            } catch (InputMismatchException e){
+                System.out.println("\n ---- Debes escribir números enteros --- \n");
+                sn.next();
+            }
+            
         }
-        
-        
+          
     }
     
 }
