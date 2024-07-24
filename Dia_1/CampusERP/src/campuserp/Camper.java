f6c227fca2cdf792bc4b1a4316dd489f7d4a7817
Paula <alejandrapmp17@gmail.com>
2024-07-23 22:45:16 -0500

    feat: :sparkles: Creacion de CRUC de trainer

--- Dia_1/CampusERP/src/campuserp/Camper.java
+++ Dia_1/CampusERP/src/campuserp/Camper.java
@@ -35,10 +35,6 @@ public class Camper {
         this.riesgo = riesgo;
     }
 
-    Camper() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
     public int getId(){
         return id;
     }
