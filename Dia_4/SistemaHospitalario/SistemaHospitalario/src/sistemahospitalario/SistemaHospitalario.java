777181f3ed296b3c4de6efe920900e35e8f9ae66
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-07-29 06:59:19 -0500

    feat: :sparkles: Prueba de Conexión

--- Dia_4/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/SistemaHospitalario.java
+++ Dia_4/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/SistemaHospitalario.java
@@ -15,6 +15,9 @@ public class SistemaHospitalario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Conexion cone = new Conexion();
+        cone.Conexion();
     }
     
 }
