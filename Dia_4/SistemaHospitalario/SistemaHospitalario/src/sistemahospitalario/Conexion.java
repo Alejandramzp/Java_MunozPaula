27fd9c276bde089285bab6bcbb0f310355bc81c6
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-07-29 06:57:20 -0500

    feat: :sparkles: Conexión de Java y MySQL

--- Dia_4/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/Conexion.java
+++ Dia_4/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/Conexion.java
@@ -6,19 +6,31 @@ package sistemahospitalario;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author camper
  */
 public class Conexion {
-    Connection con;
+    Connection con = null;
+    
+    String usuario ="root";
+    String contrasena ="Sena1234";
+    String bd ="sistema_hospitalario";
+    String ip ="localhost";
+    String puerto ="3306";
+    
+    String cadena = "jdbc:mysql://"+ip+":"+puerto+"/"+bd;
+    
     public Connection Conexion(){
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistema_hospitalario","campus2023","campus2023"); 
-        } catch (Exception e) {  
-            System.err.println("Error en la conexión:" + e);
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(cadena,usuario,contrasena); 
+            JOptionPane.showMessageDialog(null,"La conexión se realizó con exito" );
+        } catch (ClassNotFoundException | SQLException e) {  
+            JOptionPane.showMessageDialog(null,"Error al conectarse a la BBDD, error:" + e.toString());
         }
         return con;
     }
