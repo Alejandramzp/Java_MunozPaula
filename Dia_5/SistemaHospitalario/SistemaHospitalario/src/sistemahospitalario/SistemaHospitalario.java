e9d8a45f7741ee4b680766f31308d3ed5f564f46
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-07-29 22:20:22 -0500

    feat: :sparkles: Creacion de menú principal

--- Dia_5/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/SistemaHospitalario.java
+++ Dia_5/SistemaHospitalario/SistemaHospitalario/src/sistemahospitalario/SistemaHospitalario.java
@@ -4,6 +4,8 @@
  */
 package sistemahospitalario;
 
+import java.util.Scanner;
+
 /**
  *
  * @author camper
@@ -15,9 +17,37 @@ public class SistemaHospitalario {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        HospitalCRUD hospital = new HospitalCRUD();
         
-        Conexion cone = new Conexion();
-        cone.Conectar();
+        // Scanner permite obtener la entrada de datos
+        Scanner sn1 = new Scanner(System.in); 
+        boolean salir1 = false;
+        int opcion1; //Opcion del usuario
+        
+        while(!salir1){ // mientras salir sea true 
+           
+            System.out.println("------Menú-----");
+            System.out.println("1. Ver Hospital");
+            System.out.println("2. Crear Hospital");
+            System.out.println("3. Actualizar Hospital");
+            System.out.println("4. Eliminar Hospital");
+            System.out.println("5. Salir");
+            
+            System.out.println("Escriba una de las opciones");
+            opcion1 = sn1.nextInt(); //recibe el numero entero y lo compara 
+            
+            switch (opcion1){
+                
+                case 1 -> hospital.VerHospital();
+                case 2 -> hospital.InsertarHospital();
+                case 3 -> hospital.ActualizarHospital();
+                case 4 -> hospital.DeleteHospital();
+                case 5 -> {
+                System.out.println("\n --- Saliendo --- \n");
+                    salir1 = true;}
+                default -> System.out.println("\n ---- Opción Incorrecta --- \n ");
+            }
+            }
     }
     
 }
