bf5b5a8dbc239fc8bf5a21e69c9b994aeadabea8
Paula Muñoz <156429313+Alejandramzp@users.noreply.github.com>
2024-08-22 08:41:24 -0500

    feat: :sparkles: Carpeta Dia 15 Quiz

---
+++ Dia_15/QuizS/src/Logica/Visa.java
@@ -0,0 +1,16 @@
+
+package Logica;
+
+public class Visa extends Tarjeta{
+
+    public Visa(String numeroDeCuenta, double valorApertura, String mes) {
+        super(numeroDeCuenta, valorApertura, mes);
+    }
+
+    @Override
+    public double cuotaDeManejo() {
+        return 50000 - Descuento.DIAMANTE.getValorDescontado(getValorApertura());
+
+    }
+    
+}
