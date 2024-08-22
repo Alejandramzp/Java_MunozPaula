609cf619f9631652e151f6edb3b356b60ffcfee0
Paula <alejandrapmp17@gmail.com>
2024-08-22 18:04:42 -0500

    feat: :sparkles: Carpeta Dia 17

---
+++ Dia_17/BBDD_SistemaFacturacion.sql
@@ -0,0 +1,31 @@
+USE bbz9x7uffmmt980va1ni;
+
+CREATE TABLE Cliente (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    tipo VARCHAR(50)
+);
+
+CREATE TABLE Producto (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    precio DECIMAL(10, 2)
+);
+
+CREATE TABLE Factura (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    total DECIMAL(10, 2),
+    FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
+);
+
+CREATE TABLE Factura_Producto (
+    factura_id INT,
+    producto_id INT,
+    cantidad INT,
+    PRIMARY KEY (factura_id, producto_id),
+    FOREIGN KEY (factura_id) REFERENCES Factura(id),
+    FOREIGN KEY (producto_id) REFERENCES Producto(id)
+);
+
+SELECT * FROM Factura
---
+++ Dia_17/Facturacion/src/facturacion/Conexion.java
@@ -0,0 +1,40 @@
+
+package facturacion;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    private Connection con;
+    public Connection establecerConexion() {
+        Properties props = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("Config.properties")) {
+            if (input == null) {
+                throw new IllegalStateException("Archivo Config.properties no encontrado");
+            }
+
+            props.load(input);
+
+            String url = props.getProperty("URL");
+            String user = props.getProperty("USER");
+            String password = props.getProperty("PASSWORD");
+
+            if (url == null || user == null || password == null) {
+                throw new IllegalStateException("Una o más propiedades de conexión no están definidas");
+            }
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (IOException | ClassNotFoundException | SQLException | IllegalStateException e) {
+            System.err.println("Error en la conexión :(, error: " + e);
+            JOptionPane.showMessageDialog(null, "Error en la conexión: " + e.toString());
+        }
+        return con;
+    }
+}
